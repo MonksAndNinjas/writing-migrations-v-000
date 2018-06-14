0db4b650a21bba32757f2e3beb6024d455fb7a05
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-14 21:28:10 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeBirthdateToStudents < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
+#class AddGradeBirthdateToStudents < ActiveRecord::Migration[5.1]
+#  def change
+#  end
+#end
